261f462d9d15238c904f767d624710b17400eab4
Leddzip <leddzip@gmail.com>
2024-10-04 21:17:53 +0200

    new model with better input transormation

--- translator.py
+++ translator.py
@@ -5,10 +5,10 @@ def reshape(scalar, scale) -> int:
 def translate(input_list: list):
     positive_scale = sum([i for i in input_list if i >= 0])
     return {
-        "E": reshape(input_list[0], positive_scale),
-        "D": reshape(input_list[1], positive_scale),
-        "C": reshape(input_list[2], positive_scale),
-        "B": reshape(input_list[3], positive_scale),
-        "A": reshape(input_list[4], positive_scale),
-        "S": reshape(input_list[5], positive_scale)
+        "E": reshape(input_list[5], positive_scale),
+        "D": reshape(input_list[4], positive_scale),
+        "C": reshape(input_list[3], positive_scale),
+        "B": reshape(input_list[2], positive_scale),
+        "A": reshape(input_list[1], positive_scale),
+        "S": reshape(input_list[0], positive_scale)
     }
